95cf8049699bdab66364a3ef4c19d132423b710d
HemrajRijal <her0139@my.londonmet.ac.uk>
2020-06-20 21:06:15 +0545

    complete CSS and JS Clock

---
+++ Day-2 CSS & JavaScript Clock/app.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+    const now = new Date();
+
+    const second = now.getSeconds();
+    const secondDegree = ((second / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+
+    const minute = now.getMinutes();
+    const minuteDegree = ((minute / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+
+    const hour = now.getHours();
+    const hourDegree = ((hour / 12) * 360 + 90);
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+
+}
+
+setInterval(setDate, 1000)
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## 30 days Vanilla Js Challenge
 
 #### DAY-1: DRUM KIT
+#### DAY-2: CSS & JavaScript Clock
